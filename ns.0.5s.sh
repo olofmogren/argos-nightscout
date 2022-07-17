9108e3219926de3e1acc667ceeeef3919d82316b
Olof Mogren <olof@mogren.one>
2022-07-17 12:28:07 +0200

    Popup menu now works. Shows current delta.

--- ns.0.5s.sh
+++ ns.0.5s.sh
@@ -6,7 +6,6 @@ UNIT="MMOLL" # MMOLL or MGDL
 
 SGVFILE=".ns-latest-sgv.txt"
 SGVFILEHIST=".ns-latest-sgv-5mins.txt"
-#TIMEFILE=".ns-latest-sgv-time.txt"
 
 CURTIME=$(date +%s)
 MTIME=$(expr $CURTIME - 10 \* 60)
@@ -22,36 +21,24 @@ fi
 
 if test -f "$SGVFILEHIST"; then
   SGVHIST=$(cat $SGVFILEHIST)
+  SGVHISTMTIME=$(date +%s -r $SGVFILE)
 fi
 
 AGE=$(expr $CURTIME - $MTIME)
 AGEMINUTES=$(expr $AGE / 60)
 FOURMINUTESLATER=$(expr $MTIME + 4 \* 60)
-DIFF=$(python -c "print('{:d}'.format(int(10*($SGV - $SGVHIST))))")
-ARROW="🠒"
-#echo $DIFF
-if [ $DIFF -gt 12 ]; then
-  ARROW="⇈"
-elif [ $DIFF -gt 6 ]; then
-  ARROW="🠑"
-elif [ $DIFF -gt 2 ]; then
-  ARROW="⬈"
-elif [ $DIFF -ge -2 ]; then
-  ARROW="🠒"
-elif [ $DIFF -ge -6 ]; then
-  ARROW="⬊"
-elif [ $DIFF -ge -12 ]; then
-  ARROW="🠓"
-else
-  ARROW="⇊"
-fi
 
+# DECIDE PRINTSGV, PRINTAGE, COLOR
 if [ "$AGE" -lt "$UPDATEEVERY" ]; then
-  echo "$SGV $ARROW <span color='#090'>($AGEMINUTES)</span>"
+  PRINTSGV="$SGV"
+  PRINTAGE="$AGEMINUTES"
+  COLOR="#090"
 else
   RESPONSE=$(wget --quiet -O- "$HOST/api/v1/entries/sgv.json?secret=$SECRET&count=1")
   if [ -z "$RESPONSE" ]; then
-    echo "$SGV $ARROW <span color='#900'>($AGEMINUTES)</span>"
+    PRINTSGV="$SGV"
+    PRINTAGE="$AGEMINUTES"
+    COLOR="#900"
   else
     MGDL=$(echo "$RESPONSE" | jq '.[0].sgv')
     NSTIME=$(echo "$RESPONSE" | jq '.[0].dateString' | tr -d '"')
@@ -65,18 +52,57 @@ else
       NEW_SGV=$MGDL
     fi
 
-    echo "$NEW_SGV $ARROW <span color='#990'>(U)</span>"
+    PRINTSGV="$NEW_SGV"
+    PRINTAGE="U"
+    COLOR="#990"
     if [ "$NSAHEAD" -gt 0 ]; then
-    #if (( $(echo "$NEW_SGV != $SGV" | bc -l) )); then
       echo $NEW_SGV > $SGVFILE
       echo $SGV > $SGVFILEHIST
+      SGVHIST="$SGV"
       NSTIME_FORMATTED=$(date -d @$NSTIME "+%Y%m%d%H%M.%S")
-      #echo $NEW_MTIME
       # Set mtime of SGVFILE to NSTIME:
       touch -a -m -t $NSTIME_FORMATTED $SGVFILE
     fi
   fi
 fi
 
+# DECIDE DIFF
+DIFF=$(python -c "print('{:d}'.format(int(0.5+10*($PRINTSGV - $SGVHIST))))")
+ARROW="🠒"
+#echo $DIFF
+if [ $DIFF -gt 12 ]; then
+  ARROW="⇈"
+elif [ $DIFF -gt 6 ]; then
+  ARROW="🠑"
+elif [ $DIFF -gt 2 ]; then
+  ARROW="↗"
+elif [ $DIFF -ge -2 ]; then
+  ARROW="🠒"
+elif [ $DIFF -ge -6 ]; then
+  ARROW="↘"
+elif [ $DIFF -ge -12 ]; then
+  ARROW="🠓"
+else
+  ARROW="⇊"
+fi
 
+#CHECK IF SGVHISTMTIME IS VALID FOR ARROW, IF NOT, REMOVE ARROW
+if [ -z "$SGVHISTMTIME" ]; then
+  #SGVHISTMTIME NOT SET
+  ARROW=" "
+else
+  #SGVHISTMTIME SET
+  SGVHISTAGE=$(expr $CURTIME - $SGVHISTMTIME)
+  if [ "$SGVHISTAGE" -gt 600 ]; then
+    #SGVHISTMTIME MORE THAN TEN MINUTES
+    ARROW=" "
+  fi
+fi
+
+echo "$PRINTSGV $ARROW <span color='$COLOR'>($PRINTAGE)</span>"
+
+# SHOW THE DIFF IN A POPUP MENU
+PRINTDIFF=$(python -c "print('{:.1f}'.format($DIFF/10.0))")
+echo '---'
+echo "Delta: $PRINTDIFF" 
 
